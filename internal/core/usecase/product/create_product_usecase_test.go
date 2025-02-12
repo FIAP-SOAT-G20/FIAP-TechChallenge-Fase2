9adfdacf2640a6324eea72f713bdd645fda01a46
Filipe Leuch Bonfim <filipe1309@gmail.com>
2025-02-11 21:04:13 -0300

    refactor: clean up commented-out test cases in product use cases

--- internal/core/usecase/product/create_product_usecase_test.go
+++ internal/core/usecase/product/create_product_usecase_test.go
@@ -70,10 +70,6 @@ func TestCreateProductUseCase_Execute(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// t.Cleanup(func() {
-			// 	mockGateway.EXPECT().EXPECTATIONS = nil
-			// 	mockPresenter.EXPECTATIONS = nil
-			// })
 			tt.setupMocks()
 
 			err := useCase.Execute(ctx, tt.input)
