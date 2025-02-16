2243fd1565894904a5cf8ff0dcc79fea450dfb8e
Filipe Leuch Bonfim <filipe1309@gmail.com>
2025-02-16 00:28:11 -0300

    fix: reorder order_histories table drop from migration

--- internal/infrastructure/database/migrations/000001_initial.down.sql
+++ internal/infrastructure/database/migrations/000001_initial.down.sql
@@ -1,10 +1,10 @@
 DROP TABLE payments;
 DROP TABLE order_products;
 DROP TABLE products;
+DROP TABLE order_histories;
 DROP TABLE staffs;
 DROP TABLE categories;
 DROP TABLE orders;
 DROP TABLE customers;
-DROP TABLE order_histories;
 
 DROP TYPE order_status;
