332d51b04fa61c41e8a8217bc45861cd4021be7d
Filipe Leuch Bonfim <filipe1309@gmail.com>
2025-02-19 14:24:53 -0300

    refactor: add XML pagination presenter and remove duplicate definition from product response

--- internal/adapter/presenter/product_xml_response.go
+++ internal/adapter/presenter/product_xml_response.go
@@ -14,9 +14,3 @@ type ProductXmlPaginatedResponse struct {
 	XmlPagination
 	Products []ProductXmlResponse `xml:"products"`
 }
-
-type XmlPagination struct {
-	Total int64 `xml:"total" example:"100"`
-	Page  int   `xml:"page" example:"1"`
-	Limit int   `xml:"limit" example:"10"`
-}
--- internal/adapter/presenter/response_writer.go
+++ internal/adapter/presenter/response_writer.go
@@ -6,7 +6,6 @@ type ResponseWriter interface {
 	JSON(statusCode int, obj any)
 	XML(statusCode int, obj any)
 	Error(err error) *gin.Error
-	// Write(p []byte) (n int, err error)
 }
 
 type Error struct {
