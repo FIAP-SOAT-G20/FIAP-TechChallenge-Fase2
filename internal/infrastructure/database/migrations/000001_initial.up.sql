bc2285d7440397c026714f95354a184df9f02f2b
Filipe Leuch Bonfim <filipe1309@gmail.com>
2025-02-27 09:41:39 -0300

    feat: enhance initial migration with additional order and payment records for comprehensive testing

--- internal/infrastructure/database/migrations/000001_initial.up.sql
+++ internal/infrastructure/database/migrations/000001_initial.up.sql
@@ -110,14 +110,21 @@ VALUES ('Lucas Mendes', 'lucas@email.com', '123.456.789-00'),
        ('Mariana Lima', 'mariana@email.com', '789.123.456-00'),
        ('Bruno Oliveira', 'bruno@email.com', '321.654.987-00');
 
-INSERT INTO orders (customer_id, total_bill, status)
-VALUES (1, 32.80, 'OPEN'),
-       (2, 42.90, 'PENDING'),
-       (3, 25.90, 'CANCELLED'),
-       (4, 58.70, 'RECEIVED'),
-       (5, 19.80, 'PREPARING'),
-       (1, 25.90, 'READY'),
-       (2, 6.90, 'COMPLETED');
+INSERT INTO orders (id, customer_id, total_bill, status, created_at)
+VALUES (1, 1, 32.80, 'OPEN', now()),
+       (2, 2, 42.90, 'PENDING', now()),
+       (3, 3, 25.90, 'CANCELLED', '2021-10-01 10:00:00.467'),
+       (4, 4, 58.70, 'RECEIVED', '2021-10-01 10:00:00.467'),
+       (5, 5, 19.80, 'PREPARING', '2021-10-01 10:00:00.467'),
+       (6, 1, 25.90, 'READY', '2021-10-01 10:00:00.467'),
+       (7, 2, 6.90, 'COMPLETED', '2021-01-01 10:00:00.467'),
+       (8, 4, 59.70, 'RECEIVED', '2021-10-01 11:00:00.467'),
+       (9, 4, 60.70, 'RECEIVED', '2021-10-01 12:00:00.467'),
+       (10, 5, 20.80, 'PREPARING', '2021-10-01 08:00:00.467'),
+       (11, 5, 21.80, 'PREPARING', '2021-10-01 09:00:00.467'),
+       (12, 1, 26.90, 'READY', '2021-10-01 10:00:01.467'),
+       (13, 1, 27.90, 'READY', '2021-10-01 09:59:59.467'),
+       (14, 1, 28.90, 'CANCELLED', '2021-10-01 09:59:59.467');
 
 
 INSERT INTO order_products (order_id, product_id, quantity)
@@ -132,7 +139,14 @@ VALUES (1, 1, 1),
        (5, 2, 2),
        (5, 3, 1),
        (6, 1, 1),
-       (7, 2, 1);
+       (7, 2, 1),
+       (8, 1, 1),
+       (9, 2, 1),
+       (10, 1, 1),
+       (11, 2, 1),
+       (12, 1, 1),
+       (13, 2, 1);
+
 
 
 INSERT INTO order_histories (order_id, staff_id, status)
@@ -159,15 +173,51 @@ VALUES (1, null, 'OPEN'),
        (7, null, 'RECEIVED'),
        (7, 2, 'PREPARING'),
        (7, 2, 'READY'),
-       (7, 2, 'COMPLETED');
+       (7, 2, 'COMPLETED'),
+       (8, null, 'OPEN'),
+       (8, null, 'PENDING'),
+       (8, null, 'RECEIVED'),
+       (9, null, 'OPEN'),
+       (9, null, 'PENDING'),
+       (9, null, 'RECEIVED'),
+       (10, null, 'OPEN'),
+       (10, null, 'PENDING'),
+       (10, null, 'RECEIVED'),
+       (10, 1, 'PREPARING'),
+       (11, null, 'OPEN'),
+       (11, null, 'PENDING'),
+       (11, null, 'RECEIVED'),
+       (11, 1, 'PREPARING'),
+       (12, null, 'OPEN'),
+       (12, null, 'PENDING'),
+       (12, null, 'RECEIVED'),
+       (12, 1, 'PREPARING'),
+       (12, 2, 'READY'),
+       (13, null, 'OPEN'),
+       (13, null, 'PENDING'),
+       (13, null, 'RECEIVED'),
+       (13, 2, 'PREPARING'),
+       (13, 2, 'READY'),
+       (14, null, 'OPEN'),
+       (14, null, 'PENDING'),
+       (14, null, 'CANCELLED');
+       
+       
 
 INSERT INTO payments (id, status, external_payment_id, order_id, qr_data)
 VALUES (1, 'PROCESSING', '09d92b11-cd55-4a72-b2ee-7377ceefe265', 2, 'QR_DATA_345'),
-       (2, 'FAILED', 'b7fa4bee-fc25-4bb4-b948-5139af948a39', 3, 'QR_DATA_789'), 
+       (2, 'FAILED', 'b7fa4bee-fc25-4bb4-b948-5139af948a39', 3, 'QR_DATA_789'),
        (3, 'CONFIRMED', '5c272292-4ba4-41e9-83d8-dea99afe5194', 4, 'QR_DATA_123'),
        (4, 'CONFIRMED', 'ac174c5e-c9ef-4407-a3b3-bceeb4163af3', 5, 'QR_DATA_456'),
        (5, 'CONFIRMED', '09d92b11-cd55-4a72-b2ee-7377ceefe265', 6, 'QR_DATA_345'),
-       (6, 'CONFIRMED', '26e24f2a-5b00-4687-800f-a7be71104b2a', 7, 'QR_DATA_789');
+       (6, 'CONFIRMED', '26e24f2a-5b00-4687-800f-a7be71104b2b', 7, 'QR_DATA_789'),
+       (7, 'CONFIRMED', '26e24f2a-5b00-4687-800f-a7be71104b2c', 8, 'QR_DATA_790'),
+       (8, 'CONFIRMED', '26e24f2a-5b00-4687-800f-a7be71104b2d', 9, 'QR_DATA_791'),
+       (9, 'CONFIRMED', '26e24f2a-5b00-4687-800f-a7be71104b2e', 10, 'QR_DATA_792'),
+       (10, 'CONFIRMED', '26e24f2a-5b00-4687-800f-a7be71104b2f', 11, 'QR_DATA_793'),
+       (11, 'CONFIRMED', '26e24f2a-5b00-4687-800f-a7be71104b2g', 12, 'QR_DATA_794'),
+       (12, 'CONFIRMED', '26e24f2a-5b00-4687-800f-a7be71104b2h', 13, 'QR_DATA_795'),
+       (13, 'ABORTED', '26e24f2a-5b00-4687-800f-a7be71104b2i', 14, 'QR_DATA_796');
        
 
 SELECT setval('categories_id_seq', (SELECT MAX(id) FROM categories));
