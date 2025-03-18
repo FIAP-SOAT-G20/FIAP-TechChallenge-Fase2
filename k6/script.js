5e0e21dfab2fe4e7bf769e8c47909c02342a917b
Alice Tomaz <tomaz.alice@gmail.com>
2025-03-18 08:01:54 -0300

    feat: setting up k6 files

--- k6/script.js
+++ k6/script.js
@@ -2,27 +2,24 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-  vus: 10,
-  duration: '30s',
+  stages: [
+    { duration: '30s', target: 20 }, // Ramp-up para 20 usuários em 30 segundos
+    { duration: '1m', target: 20 }, // Manter 20 usuários por 1 minuto
+    { duration: '30s', target: 0 }, // Ramp-down para 0 usuários em 30 segundos 
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<500'], // 95% das requisições devem ser completadas em menos de 500ms
+    'http_req_duration{staticAsset:yes}': ['p(95)<100'], // 95% dos assets estáticos devem ser completados em menos de 100ms
+    http_req_failed: ['rate<0.01'], // Menos de 1% das requisições podem falhar
+  }
 };
 
-// export const options = {
-//     // Key configurations for avg load test in this section
-//     vus: 100,
-//     duration: '40s',
-//     thresholds: {
-//       http_req_duration: ['p(95)<250'], // 95% of requests must complete below 250ms
-//     },
-//     stages: [
-//       { duration: '5m', target: 100 }, // traffic ramp-up from 1 to 100 users over 5 minutes.
-//       { duration: '30m', target: 100 }, // stay at 100 users for 30 minutes
-//       { duration: '5m', target: 0 }, // ramp-down to 0 users
-//     ],
-//   };
-
 export default function() {
-  let baseUrl = 'http://localhost:8080/api/v1';
-  let res = http.get(`${baseUrl}/health`);
-  check(res, { "status is 200": (res) => res.status === 200 });
-  sleep(1);
+    const BASE_URL = 'https://quickpizza.grafana.com';
+    const response = http.get(`${BASE_URL}`);
+    check(response, {
+        'status is 200': (r) => r.status === 200,
+        'tempo de resposta < 200ms': (r) => r.timings.duration < 200
+    });
+    sleep(1);
 }
